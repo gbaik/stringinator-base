8e0b0060095c6c607b31917de959b018de6a9b94
gbaik <gideonbaik@gmail.com>
2017-05-08 05:21:47 -0700

    Add identity function and test

--- test/underbar/identity.test.js
+++ test/underbar/identity.test.js
@@ -2,18 +2,23 @@ const _ = require('../../underbar');
 
 describe('identity()', () => {
   it('returns null if given null', () => {
-    // Your code goes here
+    expect(_.identity(null)).toBe(null);
   });
 
   it('returns the number if given a number', () => {
-    // Your code goes here
+    expect(_.identity(1)).toBe(1);
   });
 
   it('returns the same array if given an array', () => {
-    // Your code goes here
+    const array = ['a', 'b', 'c'];
+    expect(_.identity(array)).toBe(array);
   });
 
   it('returns the same object if given an object', () => {
-    // Your code goes here
+    const object = {
+      a: 1,
+      b: 2
+    };
+    expect(_.identity(object)).toBe(object);
   });
 });
