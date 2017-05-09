88f622b0593d7bd1e6a411844492c876245b8f4b
gbaik <gideonbaik@gmail.com>
2017-05-08 17:16:24 -0700

    Add last function and test

--- test/underbar/last.test.js
+++ test/underbar/last.test.js
@@ -2,14 +2,17 @@ const _ = require('../../underbar');
 
 describe('last()', () => {
   it('returns the last element of an array', () => {
-    // Your code goes here
+    const array = ['a', 'b', 'c'];
+    expect(_.last(array)).toEqual('c');  
   });
 
   it('returns the last 2 elements of an array', () => {
-    // Your code goes here
+    const array = ['a', 'b', 'c'];
+    expect(_.last(array, 2)).toEqual(['b', 'c']);  
   });
 
   it('returns the whole array if you ask for more elements than it has', () => {
-    // Your code goes here
+    const array = ['a', 'b', 'c'];
+    expect(_.last(array, 5)).toEqual(array);  
   });
 });
