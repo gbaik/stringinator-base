b040ce8d1b09554645fe8e66f0a3853252a3b3d6
gbaik <gideonbaik@gmail.com>
2017-05-09 07:37:04 -0700

    Add some function and test

--- test/underbar/some.test.js
+++ test/underbar/some.test.js
@@ -1,12 +1,14 @@
 const _ = require('../../underbar');
 
 describe('some()', () => {
-  it('returns true if any number is odd', () => {
-    // Your code goes here
+  it('returns true if any number is even', () => {
+    const nums = [1, 2, 3, 5];
+    expect(_.some(nums, num => num % 2 === 0)).toBe(true);
   });
 
   it('returns false if no number is odd', () => {
-    // Your code goes here
+    const nums = [1, 3, 5, 7];
+    expect(_.some(nums, num => num % 2 === 0)).toBe(false);
   });
 
 });
